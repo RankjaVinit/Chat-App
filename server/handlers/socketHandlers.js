41c07b19304f044add6706943b92f88bb26d1cd9
RankjaVinit <vinupatel1982@gmail.com>
2024-12-07 22:40:15 +0530

    check

--- server/handlers/socketHandlers.js
+++ server/handlers/socketHandlers.js
@@ -2,7 +2,6 @@ module.exports = (io) => {
     io.on('connection', (socket) => {
         console.log('A user connected:', socket.id);
 
-        // Handle is not there
         // Handle disconnection
         socket.on('disconnect', () => {
             console.log('A user disconnected:', socket.id);
