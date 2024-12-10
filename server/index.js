0294129ecfd7f3db6254c707bdf3f513922c835a
RankjaVinit <vinupatel1982@gmail.com>
2024-12-10 23:10:34 +0530

    router

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const registerSocketHandlers = require("./handlers");
 const User = require('./schema/User');
 const Group = require('./schema/Group');
 const Personal = require('./schema/Personal');
+const apiRoutes = require('./routes/apiRoutes');
 const getISTDate = require('./date');
 
 
@@ -25,7 +26,7 @@ const io = new Server(server);
 
 
 // Setup Express routes
-app.use('/api', apiRoutes);
+app.use('/', apiRoutes);
 
 
 
--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-// Example API endpoint
-// router.get('/status', (req, res) => {
-//     res.json({ status: 'Server is running', timestamp: new Date() });
-// });
+const User = require('../schema/User');
 
-// router.post('/message', (req, res) => {
-//     const { user, message } = req.body;
-//     if (!user || !message) {
-//         return res.status(400).json({ error: 'Missing user or message' });
-//     }
-//     res.json({ success: true, user, message });
-// });
+// to get all users
+router.get('/users', async (req, res) => {
+    let ans = await User.find();
+    res.send(ans);
+});
+
 
 module.exports = router;
